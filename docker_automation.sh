2f6a4205fde1d2a96f9bb64a83b4fd43b0d09898
vanthiyadhevan <samvickey96@gmail.com>
2025-12-17 15:02:55 +0530

    Added Jenkins installation in .sh file

--- docker_automation.sh
+++ docker_automation.sh
@@ -23,3 +23,24 @@ sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin dock
 sudo usermod -aG docker ubuntu
 
 newgrp docker
+
+# ------------------------------------
+# Jenkins install Automation
+# ------------------------------------
+sudo apt-get update
+
+sudo apt install openjdk-21-jdk -y
+
+# install maven and unzip
+sudo apt install maven unzip -y
+
+# set jenkins key 
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# update the reposirory and install jenkins
+sudo apt-get update
+sudo apt-get install jenkins -y
